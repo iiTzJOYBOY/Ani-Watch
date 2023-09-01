bae77dc57cbd9ccbd62d063847ea085afb7fea32
Aditya Bhosale <74899393+iiTzJOYBOY@users.noreply.github.com>
2023-09-01 15:43:54 +0530

    Inital Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# from flask import Flask, redirect, url_for, render_template, request
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def wellcome():
+#     return render_template('index.html')
+
+# @app.route('/pass/<int:number>')
+# def if_pass(number):
+#     return f"STUDENT GET {number}/100 SO THE STUDET PASS...!!"
+
+# @app.route('/fail/<int:number>')
+# def if_fail(number):
+#     return f"STUDENT GET {number}/100 SO THE STUDET fail...!!"
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
